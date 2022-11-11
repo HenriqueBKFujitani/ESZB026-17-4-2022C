bfe4bccab9e1d2918e287b6a80da2f4d2b216dea
pedronaba <78627281+pedronaba@users.noreply.github.com>
2022-11-11 10:27:29 -0300

    Update and rename alarme.c to alarme_media_prioridade.c

--- lab05/alarme_media_prioridade.c
+++ lab05/alarme_media_prioridade.c
@@ -10,12 +10,12 @@ int main() {                            // este programa deve ser rodado com 'su
 
    // Ajustando a frequencia do PWM em 10kHz com 128 passos de duty cycle
    // frequencia PWM = 19,2 MHz / (divisor * range)
-   // 10000 = 19200000 / (divisor * 128) => divisor = 15
+   // 261,6 = 19200000 / (divisor * 129) => divisor = 569
    pwmSetMode(PWM_MODE_MS);             // usando frequencia fixa
    pwmSetRange(129);                    // passos do duty cycle (max=4096)
    pwmSetClock(569);                     // fornece uma frequencia de 261.6Hz (max=4096)
  
-   for(ciclos = 0; ciclos < 4; ciclos++){  // variando o duty cycle
+   for(ciclos = 0; ciclos < 3; ciclos++){  // variando o duty cycle
       for(dc = 0; dc < 129; dc++){
          pwmWrite(pino_PWM0, dc);
          usleep(10000);
